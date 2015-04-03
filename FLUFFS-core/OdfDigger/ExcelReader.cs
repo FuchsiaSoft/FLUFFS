91362a80a310b53bcb31492d893f3640eb80e7a5
fuschia <dupoint17@gmail.com>
2015-04-03 14:01:18 +0100

    more drafting
    more drafting of classes around ODF

--- FLUFFS-core/OdfDigger/ExcelReader.cs
+++ FLUFFS-core/OdfDigger/ExcelReader.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace OdfDigger
 {
+    /// <summary>
+    /// Provides an implementation of IOdfReader
+    /// specifically for Excel format files.
+    /// </summary>
     internal class ExcelReader : IOdfReader
     {
         public string ReadContents(string path)
         {
-            throw new NotImplementedException();
+
+            /*
+             * Excel files are slightly more complex than Word
+             * files for parsing.  For efficiency they store
+             * strings in a shared XML file regardless of
+             * worksheet location.  This is easy to retrieve and is
+             * treated the same as for Word.  But formulas and non
+             * text based values are stored in individual XML files
+             * for each worksheet, in the xl\worksheets folder.
+             */ 
+
+            //TODO: in line with comment above, make this support
+            //more than the shared string extraction.
+
+            using (OdfUnpacker packer = new OdfUnpacker())
+            {
+                string newFolder = packer.UnpackOdf(path);
+                string sharedXmlLocation = newFolder + "\\sharedStrings.xml";
+
+            }
         }
     }
 }
