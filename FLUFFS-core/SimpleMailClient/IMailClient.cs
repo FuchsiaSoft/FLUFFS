1e729db051c99611706dd373b5ce065e1dc9c93f
Chris Wilson <dupoint17@gmail.com>
2015-04-05 16:17:18 +0100

    Added Mail Client
    fixed #8

---
+++ FLUFFS-core/SimpleMailClient/IMailClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMailClient
+{
+    public interface IMailClient
+    {
+        void SendEmail(IEnumerable<string> recipients, string subject,
+            string body, string sender);
+    }
+}
---
+++ FLUFFS-core/SimpleMailClient/MailClient.cs
@@ -0,0 +1,33 @@
+﻿using SimpleMailClient.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMailClient
+{
+    public class MailClient : IMailClient
+    {
+        public void SendEmail(IEnumerable<string> recipients, 
+            string subject, string body, string sender)
+        {
+            SmtpClient client = new SmtpClient();
+            string host = Settings.Default.SmtpHost;
+            int port = Settings.Default.SmtpPort;
+            MailMessage message = new MailMessage()
+            {
+                From = new MailAddress(sender),
+                Sender = new MailAddress(sender),
+                Subject = subject,
+                Body = body
+            };
+            foreach (string recipient in recipients)
+            {
+                message.To.Add(new MailAddress(recipient));
+            }
+            client.Send(message);
+        }
+    }
+}
