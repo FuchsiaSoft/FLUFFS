de82402c25dd7c8f28bebebd7d78c88812f1462a
fuschia <dupoint17@gmail.com>
2015-04-07 15:21:03 +0100

    sketching out BinaryReader library
    shell interface and classes for binary reader.

---
+++ FLUFFS-core/BinaryDigger/WordReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryDigger
+{
+    class WordReader : IBinaryReader
+    {
+        public string ReadContents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
