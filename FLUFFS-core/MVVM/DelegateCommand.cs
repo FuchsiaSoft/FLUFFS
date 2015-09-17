ca21d3d4541765896b1f4cabdfb8fd74a730f59a
Chris Wilson <dupoint17@gmail.com>
2015-09-17 14:36:11 +0100

    Refactoring MVVM into separate library for UI re-use

---
+++ FLUFFS-core/MVVM/DelegateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action _Action;
+
+        public DelegateCommand(Action action)
+        {
+            _Action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _Action.Invoke();
+        }
+    }
+
+}
---
+++ FLUFFS-core/MVVM/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
