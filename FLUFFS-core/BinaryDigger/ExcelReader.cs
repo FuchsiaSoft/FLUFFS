e0256bb8c08b1b583895428abd11d46a20b7c190
Chris Wilson <dupoint17@gmail.com>
2016-01-04 14:26:17 +0000

    Excel Performance changes
    Altered Excel code to remove multiple calls to AsDataSet when processing
    a spreadsheet that has multiple worksheets.

--- FLUFFS-core/BinaryDigger/ExcelReader.cs
+++ FLUFFS-core/BinaryDigger/ExcelReader.cs
@@ -29,42 +29,30 @@ namespace BinaryDigger
         /// <returns>Contents of any 2003 and prior Excel files (.xls) as a single string</returns>
         public override string ReadContents()
         {
-            
             Stream stream = new MemoryStream(File.ReadAllBytes(_FilePath));
-            IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
 
-            // get all the worksheets in the spreadsheet
-            List<string> sheets = getWorkSheetNames(excelReader).ToList<string>();
-
+            DataTableCollection worksheets;
             StringBuilder sb = new StringBuilder();
-            foreach (var sheet in sheets)
+
+            using (IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream))
             {
-                var workSheet = excelReader.AsDataSet().Tables[sheet];
-                var rows = from DataRow a in workSheet.Rows select a;
-                foreach (var row in rows)
+                worksheets = excelReader.AsDataSet().Tables;
+                excelReader.Close();
+            }
+
+            foreach (DataTable sheet in worksheets)
+            {
+                foreach (DataRow row in sheet.Rows)
                 {
-                    for (int i = 0; i <= row.ItemArray.GetUpperBound(0); i++)
+                    foreach (var cell in row.ItemArray)
                     {
-                        sb.Append(row[i].ToString());
+                        sb.Append(cell.ToString());
                     }
                 }
             }
 
-            excelReader.Close();
-
             return sb.ToString();
         }
 
-        /// <summary>
-        /// An excel file can contain multiple worksheets.  This gets the name of 
-        /// each worksheet in the file
-        /// </summary>
-        /// <param name="reader">The reader.</param>
-        /// <returns>The names of each worksheet</returns>
-        public IEnumerable<string> getWorkSheetNames(IExcelDataReader reader)
-        {
-            var sheets = from DataTable sheet in reader.AsDataSet().Tables select sheet.TableName;
-            return sheets;
-        }
     }
 }
